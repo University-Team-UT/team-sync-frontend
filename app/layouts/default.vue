58fe9fff96c08fe187a40f240bfa6c176f5737b4
up <iurykolgin@gmail.com>
2025-03-13 21:51:57 +0700

    feat: добавлен модуль для header

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,18 +1,15 @@
 <script setup lang="ts">
-import  SearchInput from '~/modules/common/search/SearchInput.vue';
-
+import AppHeader from '~/modules/header/AppHeader.vue'
 </script>
 
 <template>
 	<div class="flex">
 		<aside class="bg-green-400 fixed inset-y-0 w-62">1</aside>
+		<AppHeader />
 
-		<header class="w-full h-16 bg-[#252527] ml-62 fixed inst-x-0 flex">
-			<SearchInput class="my-auto" />
-		</header>
 		<div class="flex flex-col mt-16 ml-62 w-full">
 			<main>
-				<slot /> 
+				<slot />
 			</main>
 		</div>
 	</div>
---
+++ app/modules/header/AppHeader.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import SearchInput from '~/modules/common/search/SearchInput.vue'
+</script>
+
+<template>
+	<header class="w-full h-16 bg-[#252527] ml-62 fixed inst-x-0 flex">
+		<SearchInput class="my-auto" />
+	</header>
+</template>
+
+<style scoped></style>
