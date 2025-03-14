971545c204681e9e2918bc2384acb0704719c28e
up <iurykolgin@gmail.com>
2025-03-14 20:31:13 +0700

    test: Картинка перенесена в assets

--- app/modules/auth/AuthForm.vue
+++ app/modules/auth/AuthForm.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-	<div>
-		<img src="public/img/image 1.png" alt="Background">
-	</div>
+	<div></div>
 </template>
 
 <style scoped></style>
--- app/pages/auth.vue
+++ app/pages/auth.vue
@@ -1,7 +1,9 @@
 <script lang="ts" setup></script>
 
 <template>
-	<div class="fixed inset-0 bg-[url(/public/img/image-1.png)]">123</div>
+	<div class="fixed inset-0 bg-[url(/assets/images/background-image.png)]">
+		123
+	</div>
 </template>
 
 <style scoped></style>
