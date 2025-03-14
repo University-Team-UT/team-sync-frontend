b772dff23e11a10ff96ea6382ce9a94694a87644
up <iurykolgin@gmail.com>
2025-03-15 01:20:42 +0700

    fix: header

--- app/modules/header/AppHeader.vue
+++ app/modules/header/AppHeader.vue
@@ -81,110 +81,197 @@ const items = ref([
 								>
 							</div>
 						</div>
+						<div class="flex flex-col mt-5 w-full gap-4">
+							<div class="flex gap-8">
+								<UInput
+									:placeholder="t('invite.footer.mail')"
+									size="lg"
+									class="w-full"
+									variant="soft"
+								/>
+								<UPopover
+									:content="{
+										align: 'end',
+										side: 'bottom',
+										sideOffset: 8
+									}"
+								>
+									<UButton
+										:ui="{
+											base: 'px-6 '
+										}"
+										icon="lucide:handshake"
+										:label="t('invite.footer.role')"
+										color="neutral"
+										variant="soft"
+									/>
+
+									<template #content>
+										<div
+											class="grid items-center justify-center grid-cols-8 gap-x-4"
+										>
+											<div
+												class="col-span-2 flex flex-col items-center justify-center bg-root-800 p-4 px-6 rounded-lg"
+											>
+												<UIcon name="lucide:crown" class="h-4 w-4" />
+											</div>
+
+											<div
+												class="flex col-span-6 flex-col items-start justify-center p-2 gap-1"
+											>
+												<H1 class="text-root-100 text-sm">Админ супер</H1>
+												<p class="text-root-600 text-xs">
+													Управляет настройками проекта в целом
+												</p>
+											</div>
+
+											<div
+												class="col-span-2 flex flex-col items-center justify-center bg-root-800 p-4 px-6 rounded-lg"
+											>
+												<UIcon name="lucide:flame" class="h-4 w-4" />
+											</div>
+
+											<div
+												class="col-span-6 flex flex-col items-start p-2 rounded-lg gap-1"
+											>
+												<H1 class="text-root-100">Админ </H1>
+												<p class="text-root-600 text-xs">
+													Управляет настройками
+												</p>
+											</div>
+
+											<div
+												class="col-span-2 flex flex-col items-center justify-center bg-root-800 p-4 px-6 rounded-lg"
+											>
+												<UIcon name="lucide:handshake" class="h-4 w-4" />
+											</div>
+
+											<div
+												class="col-span-6 flex flex-col items-start p-2 rounded-lg gap-1"
+											>
+												<H1 class="text-root-100">Участник </H1>
+												<p class="text-root-600 text-xs">
+													Работает и может приглашать других
+												</p>
+											</div>
+										</div>
+									</template>
+								</UPopover>
+								<UButton icon="lucide:x" color="neutral" variant="link" />
+							</div>
+							<UButton
+								icon="lucide:circle-fading-plus"
+								size="md"
+								class="text-root-400"
+								variant="link"
+								>{{ t('invite.footer.more') }}</UButton
+							>
+						</div>
 					</template>
 
-					<template #guest> 2 </template>
+					<template #guest>
+						<div class="flex flex-col w-full gap-4">
+							<div class="flex gap-8">
+								<UInput
+									:placeholder="t('invite.footer.mail')"
+									size="lg"
+									class="w-full"
+									variant="soft"
+								/>
+								<UPopover
+									:content="{
+										align: 'end',
+										side: 'bottom',
+										sideOffset: 8
+									}"
+								>
+									<UButton
+										:ui="{
+											base: 'px-6 '
+										}"
+										icon="lucide:handshake"
+										:label="t('invite.footer.role')"
+										color="neutral"
+										variant="soft"
+									/>
+
+									<template #content>
+										<div
+											class="grid items-center justify-center grid-cols-8 gap-x-4"
+										>
+											<div
+												class="col-span-2 flex flex-col items-center justify-center bg-root-800 p-4 px-6 rounded-lg"
+											>
+												<UIcon name="lucide:crown" class="h-4 w-4" />
+											</div>
+
+											<div
+												class="flex col-span-6 flex-col items-start justify-center p-2 gap-1"
+											>
+												<H1 class="text-root-100 text-sm">Админ супер</H1>
+												<p class="text-root-600 text-xs">
+													Управляет настройками проекта в целом
+												</p>
+											</div>
+
+											<div
+												class="col-span-2 flex flex-col items-center justify-center bg-root-800 p-4 px-6 rounded-lg"
+											>
+												<UIcon name="lucide:flame" class="h-4 w-4" />
+											</div>
+
+											<div
+												class="col-span-6 flex flex-col items-start p-2 rounded-lg gap-1"
+											>
+												<H1 class="text-root-100">Админ </H1>
+												<p class="text-root-600 text-xs">
+													Управляет настройками
+												</p>
+											</div>
+
+											<div
+												class="col-span-2 flex flex-col items-center justify-center bg-root-800 p-4 px-6 rounded-lg"
+											>
+												<UIcon name="lucide:handshake" class="h-4 w-4" />
+											</div>
+
+											<div
+												class="col-span-6 flex flex-col items-start p-2 rounded-lg gap-1"
+											>
+												<H1 class="text-root-100">Участник </H1>
+												<p class="text-root-600 text-xs">
+													Работает и может приглашать других
+												</p>
+											</div>
+										</div>
+									</template>
+								</UPopover>
+								<UButton icon="lucide:x" color="neutral" variant="link" />
+							</div>
+							<UButton
+								icon="lucide:circle-fading-plus"
+								size="md"
+								class="text-root-400"
+								variant="link"
+								>{{ t('invite.footer.more') }}</UButton
+							>
+						</div></template
+					>
 				</UTabs>
 			</template>
 
 			<template #footer>
-				<div class="flex flex-col w-full gap-4">
-					<div class="flex gap-8">
-						<UInput
-							:placeholder="t('invite.footer.mail')"
-							size="lg"
-							class="w-full"
-							variant="soft"
-						/>
-						<UPopover
-							:content="{
-								align: 'end',
-								side: 'bottom',
-								sideOffset: 8
-							}"
-						>
-							<UButton
-								:ui="{
-									base: 'px-6 '
-								}"
-								icon="lucide:handshake"
-								:label="t('invite.footer.role')"
-								color="neutral"
-								variant="soft"
-							/>
-
-							<template #content>
-								<Placeholder class="max-w-56 m-4 flex flex-col">
-									<div
-										class="grid items-center justify-center grid-cols-8 gap-x-4"
-									>
-										<div
-											class="col-span-2 flex flex-col items-center justify-center bg-root-800 p-4 px-6 rounded-lg"
-										>
-											<UIcon name="lucide:crown" class="h-4 w-4" />
-										</div>
-
-										<div
-											class="flex col-span-6 flex-col items-start justify-center p-2 gap-1"
-										>
-											<H1 class="text-root-100 text-sm">Админ супер</H1>
-											<p class="text-root-600 text-xs">
-												Управляет настройками проекта в целом
-											</p>
-										</div>
-
-										<div
-											class="col-span-2 flex flex-col items-center justify-center bg-root-800 p-4 px-6 rounded-lg"
-										>
-											<UIcon name="lucide:flame" class="h-4 w-4" />
-										</div>
-
-										<div
-											class="col-span-6 flex flex-col items-start p-2 rounded-lg gap-1"
-										>
-											<H1 class="text-root-100">Админ </H1>
-											<p class="text-root-600 text-xs">Управляет настройками</p>
-										</div>
-
-										<div
-											class="col-span-2 flex flex-col items-center justify-center bg-root-800 p-4 px-6 rounded-lg"
-										>
-											<UIcon name="lucide:handshake" class="h-4 w-4" />
-										</div>
-
-										<div
-											class="col-span-6 flex flex-col items-start p-2 rounded-lg gap-1"
-										>
-											<H1 class="text-root-100">Участник </H1>
-											<p class="text-root-600 text-xs">
-												Работает и может приглашать других
-											</p>
-										</div>
-									</div>
-								</Placeholder>
-							</template>
-						</UPopover>
-						<UButton icon="lucide:x" color="neutral" variant="link" />
-					</div>
+				<div class="flex justify-end w-full gap-2">
 					<UButton
-						icon="lucide:circle-fading-plus"
-						size="md"
-						class="text-root-400"
-						variant="link"
-						>{{ t('invite.footer.more') }}</UButton
-					>
-					<div class="flex justify-end w-full gap-2">
-						<UButton
-							color="primary"
-							:label="t('invite.footer.cancel')"
-							variant="outline"
-						/>
-						<UButton
-							color="primary"
-							:label="t('invite.footer.send')"
-							variant="ghost"
-						/>
-					</div>
+						color="primary"
+						:label="t('invite.footer.cancel')"
+						variant="outline"
+					/>
+					<UButton
+						color="primary"
+						:label="t('invite.footer.send')"
+						variant="ghost"
+					/>
 				</div>
 			</template>
 		</UModal>
