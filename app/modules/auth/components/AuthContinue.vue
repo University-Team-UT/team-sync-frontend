733493ab43247c9c6576537d5078fa3fe772c63b
Kostya <kostyanaumencko@yandex.ru>
2025-03-19 20:56:51 +0700

    feat: Добавил компоненты иконок флагов

--- app/modules/auth/components/AuthContinue.vue
+++ app/modules/auth/components/AuthContinue.vue
@@ -3,6 +3,15 @@ const emit = defineEmits(['setPageTab'])
 </script>
 
 <template>
+	<AppLogo />
+	<UAlert
+		class="text-wrap"
+		title="Продолжить с Почтой"
+		description="Создайте аккаунт через электронную почту"
+		icon="i-lucide-terminal"
+		variant="outline"
+		color="primary"
+	/>
 	<div @click="emit('setPageTab', 'start')">123</div>
 </template>
 
