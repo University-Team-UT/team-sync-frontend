0c5396eaec4dd245bf6f5773da91121fb960761b
up <iurykolgin@gmail.com>
2025-03-25 22:32:58 +0700

    refactor: изменен хедер

---
+++ app/modules/common/invitation/InviteLinkCreation.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+const { t } = useI18n()
+</script>
+
+<template>
+	<div class="flex items-center">
+		<p>{{ t('invite.tabs.content.title') }}</p>
+		<UTooltip
+			arrow
+			:text="t('invite.tabs.content.tooltip')"
+			:ui="{
+				content: 'h-auto max-w-68',
+				text: 'text-wrap text-justify'
+			}"
+			:content="{
+				align: 'start',
+				side: 'bottom',
+				sideOffset: 8
+			}"
+		>
+			<UButton
+				icon="lucide:circle-help"
+				color="neutral"
+				variant="link"
+				class="hover:text-primary-400"
+				size="sm"
+			/>
+		</UTooltip>
+		<USwitch default-value size="sm" />
+	</div>
+	<div class="flex gap-5 justify-between">
+		<UButtonGroup class="w-full">
+			<UInput
+				color="neutral"
+				variant="soft"
+				:placeholder="t('invite.link')"
+				class="w-full"
+			/>
+
+			<UButton color="neutral" variant="soft" icon="lucide-files" />
+		</UButtonGroup>
+		<UButton color="primary" class="font-bold self-center px-7" size="sm">{{
+			t('invite.copy')
+		}}</UButton>
+	</div>
+</template>
+
+<style scoped></style>
---
+++ app/modules/common/invitation/types.ts
@@ -0,0 +1,7 @@
+import type { BaseRoles } from '~/types/common.types'
+
+export interface IRoleItemProps {
+	role: BaseRoles
+	desc: string
+	icon: string
+}
