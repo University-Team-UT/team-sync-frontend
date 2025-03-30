935c517fbd61c7a3beb32a0e8f2fc2a163631a82
up-11 <iurykolgin@gmail.com>
2025-03-30 22:38:54 +0700

    fix: Ссылки теперь учитывают локаль

--- src/modules/sidebar/SidebarItem.vue
+++ src/modules/sidebar/SidebarItem.vue
@@ -10,15 +10,16 @@ const buttonText = computed(() => (!props.isCollapsed ? props.text : ''))
 </script>
 
 <template>
-	<UButton
-		:class="[
-			'bg-transparent hover:bg-root-800 w-full',
-			isCollapsed && 'justify-center'
-		]"
-		:to="to"
-		variant="soft"
-		color="neutral"
-		:icon="icon"
-		>{{ buttonText }}</UButton
-	>
+	<NuxtLinkLocale :to="to">
+		<UButton
+			:class="[
+				'bg-transparent hover:bg-root-800 w-full',
+				isCollapsed && 'justify-center'
+			]"
+			variant="soft"
+			color="neutral"
+			:icon="icon"
+			>{{ buttonText }}</UButton
+		>
+	</NuxtLinkLocale>
 </template>
